77b98ae3c03ccaab533110503eef769279758d1c
wsigma21 <wallstlbl@gmail.com>
2024-01-11 20:23:07 +0900

    feat: Header作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { Header } from "./components/header/Header";
+
 function App() {
   return (
-    <div>
-      <p></p>
+    <div className="app">
+      <Header />
     </div>
   );
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <h1>Simple Trello</h1>
+    </header>
+  )
+}
