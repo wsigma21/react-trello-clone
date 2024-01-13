55d60954d7349280a3b30480dc7d4a699f332719
wsigma21 <wallstlbl@gmail.com>
2024-01-13 12:14:32 +0900

    feat: TaskCard削除機能

--- src/components/task/button/TaskCardDeleteButton.jsx
+++ src/components/task/button/TaskCardDeleteButton.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-export const TaskCardDeleteButton = () => {
+export const TaskCardDeleteButton = ({ taskCardsList, setTaskCardsList, taskCard }) => {
+  const TaskCardDeleteButton = (id) => {
+    // タスクカードを削除する
+    setTaskCardsList(taskCardsList.filter((e) => e.id !== id));
+  }
   return (
-    <div>TaskCardDeleteButton</div>
+    <div>
+      <button className="taskCardDeleteButton" onClick={() => TaskCardDeleteButton(taskCard.id)}>
+        <i className="fa-regular fa-circle-xmark"></i>
+      </button>
+    </div>
   )
 }
